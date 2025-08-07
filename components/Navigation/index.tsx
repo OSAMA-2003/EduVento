6bc75c55534a586909dd9aaafc687de66563c292
Osama <osama22@gmail.com>
2025-08-07 22:07:08 +0300

    edit menu

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -102,8 +102,16 @@ const Navigation = () => {
                     <li><Link href="/partners" className="block px-6 py-3 hover:bg-secondary-green/20 transition-colors hover:text-logo-blue">شركاؤنا في التعليم</Link></li>
                     <li><Link href="/instructors" className="block px-6 py-3 hover:bg-primary-yellow/20 transition-colors hover:text-logo-blue">المدربين المعتمدين</Link></li>
                     <li><Link href="/ambassadors" className="block px-6 py-3 hover:bg-logo-blue/10 transition-colors hover:text-logo-blue">سفراء Eduvento</Link></li>
-                    <li className="relative" onMouseEnter={() => setJoinOpen2(true)} onMouseLeave={() => setJoinOpen2(false)}>
-                      <button className="w-full font-bold text-right  text-logo-blue px-6 py-3 flex items-center gap-1" onClick={() => setJoinOpen2((v) => !v)}>
+                   
+                  </motion.ul>
+                )}
+              </AnimatePresence>
+
+              
+               
+            </div>
+            <div className="relative" onMouseEnter={() => setJoinOpen2(true)} onMouseLeave={() => setJoinOpen2(false)}>
+                      <button className="font-medium text-white hover:text-logo-blue text-lg flex items-center gap-1" onClick={() => setJoinOpen2((v) => !v)}>
                         فرص التعاون
                         <svg className={`w-4 h-4 ml-1 ${joinOpen2 ? 'rotate-180' : ''}`} fill="none" viewBox="0 0 24 24">
                           <path d="M19 9l-7 7-7-7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -125,16 +133,25 @@ const Navigation = () => {
                           </motion.ul>
                         )}
                       </AnimatePresence>
-                    </li>
-                  </motion.ul>
-                )}
-              </AnimatePresence>
-            </div>
+                    </div>
           </div>
 
-          <button onClick={() => setIsOpen(!isOpen)} className="md:hidden p-2 rounded-lg text-white">
-            {isOpen ? <X className="h-7 w-7" /> : <Menu className="h-7 w-7" />}
-          </button>
+         <label className="hamburger md:hidden">
+  <input
+    type="checkbox"
+    checked={isOpen}
+    onChange={() => setIsOpen(!isOpen)}
+    aria-label="Toggle menu"
+  />
+  <svg viewBox="0 0 32 32">
+    <path
+      className="line line-top-bottom"
+      d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+    />
+    <path className="line" d="M7 16 27 16" />
+  </svg>
+</label>
+
         </div>
 
         <AnimatePresence>
@@ -168,7 +185,14 @@ const Navigation = () => {
                         <Link href="/partners" onClick={() => setIsOpen(false)} className="block text-md">شركاؤنا في التعليم</Link>
                         <Link href="/instructors" onClick={() => setIsOpen(false)} className="block text-md">المدربين المعتمدين</Link>
                         <Link href="/ambassadors" onClick={() => setIsOpen(false)} className="block text-md">سفراء Eduvento</Link>
-                        <button className="text-xl w-full font-bold text-yellow-200 text-right flex justify-between items-center" onClick={() => setJoinOpen2(!joinOpen2)}>
+                       
+                      </motion.div>
+                    )}
+
+                  </AnimatePresence>
+                   
+                </motion.div>
+                <button className="text-xl w-full font-bold text-yellow-200 text-right flex justify-between items-center" onClick={() => setJoinOpen2(!joinOpen2)}>
                           فرص التعاون
                           <svg className={`w-4 h-4 transition-transform ${joinOpen2 ? 'rotate-180' : ''}`} fill="none" viewBox="0 0 24 24">
                             <path d="M19 9l-7 7-7-7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -185,10 +209,6 @@ const Navigation = () => {
                             </motion.div>
                           )}
                         </AnimatePresence>
-                      </motion.div>
-                    )}
-                  </AnimatePresence>
-                </motion.div>
               </motion.div>
             </motion.div>
           )}
