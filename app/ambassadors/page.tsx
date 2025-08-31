1825b058f4226ee964a55eb9c99a2a177683882e
Osama <osama22@gmail.com>
2025-08-31 16:25:55 +0300

    test ampasadors

--- app/ambassadors/page.tsx
+++ app/ambassadors/page.tsx
@@ -9,6 +9,8 @@ export const metadata: Metadata = {
 };
 
 
+export const dynamic = "force-dynamic";
+
 export default async function AmbassadorsPage() {
   const ambassadors = await fetchAllAmbassadors();
   console.log(ambassadors);
